ff07c5126b8175b7a0e788f1af640a612cd5e929
255640 <255640@PC107027>
2025-11-26 11:25:26 +0000

    hello

--- Foodreviews/Form1.Designer.cs
+++ Foodreviews/Form1.Designer.cs
@@ -28,33 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.enterUsername = new System.Windows.Forms.TextBox();
+            this.enterPassword = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.restaurantType = new System.Windows.Forms.ComboBox();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // enterUsername
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(621, 80);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.enterUsername.Location = new System.Drawing.Point(999, 27);
+            this.enterUsername.Name = "enterUsername";
+            this.enterUsername.Size = new System.Drawing.Size(100, 26);
+            this.enterUsername.TabIndex = 1;
+            // 
+            // enterPassword
+            // 
+            this.enterPassword.Location = new System.Drawing.Point(1105, 28);
+            this.enterPassword.Name = "enterPassword";
+            this.enterPassword.Size = new System.Drawing.Size(100, 26);
+            this.enterPassword.TabIndex = 2;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.IndianRed;
+            this.loginButton.Location = new System.Drawing.Point(1226, 25);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 30);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // restaurantType
+            // 
+            this.restaurantType.FormattingEnabled = true;
+            this.restaurantType.Location = new System.Drawing.Point(51, 3);
+            this.restaurantType.Name = "restaurantType";
+            this.restaurantType.Size = new System.Drawing.Size(551, 28);
+            this.restaurantType.TabIndex = 0;
+            this.restaurantType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.restaurantType);
+            this.panelMain.Location = new System.Drawing.Point(232, 25);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(670, 44);
+            this.panelMain.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1352, 889);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.enterPassword);
+            this.Controls.Add(this.enterUsername);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMain.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox enterUsername;
+        private System.Windows.Forms.TextBox enterPassword;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.ComboBox restaurantType;
+        private System.Windows.Forms.Panel panelMain;
     }
 }
 
--- Foodreviews/Form1.cs
+++ Foodreviews/Form1.cs
@@ -6,20 +6,58 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Foodreviews
 {
     public partial class Form1 : Form
     {
+        private const string CONNECT = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"M:\\Visual Studio 2022\\FOOD REVIEWS\\Foodreviews\\Foodreviews\\Food Reviews DDL.mdf\";Integrated Security=True;Connect Timeout=30";
+        private SqlConnection sqlConnection;
+
         public Form1()
         {
             InitializeComponent();
+            sqlConnection = new SqlConnection(CONNECT);
+        }
+
+        private void GetTags()
+        {
+            sqlConnection.Open();
+
+            //setup query
+            SqlCommand command = new SqlCommand("SELECT Tag FROM Tags ORDER BY Tag", sqlConnection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    restaurantType.Items.Add(reader["Tag"]);
+ 
+                }
+            }
+            sqlConnection.Close();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if(enterUsername.Text == "admin" &&  enterPassword.Text == "1234")
+            {
+                panelMain.Enabled = true;
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetTags();
+            panelMain.Enabled = false;
         }
     }
 }
