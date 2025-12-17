a723291ad6aa465f6ed6d062ccbf450fea6a31e2
255640 <255640@PC107027>
2025-12-17 11:25:51 +0000

    e ghtfrhrt

--- Foodreviews/Form1.Designer.cs
+++ Foodreviews/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.loginButton = new System.Windows.Forms.Button();
             this.restaurantType = new System.Windows.Forms.ComboBox();
             this.panelMain = new System.Windows.Forms.Panel();
+            this.restaurantNamesList = new System.Windows.Forms.ListBox();
+            this.incorrectPasswordWarning = new System.Windows.Forms.TextBox();
+            this.checkedListBoxRN = new System.Windows.Forms.CheckedListBox();
             this.panelMain.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,6 +45,7 @@
             this.enterUsername.Name = "enterUsername";
             this.enterUsername.Size = new System.Drawing.Size(100, 26);
             this.enterUsername.TabIndex = 1;
+            this.enterUsername.TextChanged += new System.EventHandler(this.enterUsername_TextChanged);
             // 
             // enterPassword
             // 
@@ -69,6 +73,7 @@
             this.restaurantType.Size = new System.Drawing.Size(551, 28);
             this.restaurantType.TabIndex = 0;
             this.restaurantType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.restaurantType.Click += new System.EventHandler(this.restaurantType_Click);
             // 
             // panelMain
             // 
@@ -78,11 +83,47 @@
             this.panelMain.Size = new System.Drawing.Size(670, 44);
             this.panelMain.TabIndex = 4;
             // 
+            // restaurantNamesList
+            // 
+            this.restaurantNamesList.FormattingEnabled = true;
+            this.restaurantNamesList.ItemHeight = 20;
+            this.restaurantNamesList.Location = new System.Drawing.Point(1302, 873);
+            this.restaurantNamesList.Name = "restaurantNamesList";
+            this.restaurantNamesList.Size = new System.Drawing.Size(26, 24);
+            this.restaurantNamesList.TabIndex = 5;
+            this.restaurantNamesList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // incorrectPasswordWarning
+            // 
+            this.incorrectPasswordWarning.BackColor = System.Drawing.SystemColors.Control;
+            this.incorrectPasswordWarning.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.incorrectPasswordWarning.ForeColor = System.Drawing.Color.Red;
+            this.incorrectPasswordWarning.Location = new System.Drawing.Point(1068, 60);
+            this.incorrectPasswordWarning.Margin = new System.Windows.Forms.Padding(0);
+            this.incorrectPasswordWarning.Name = "incorrectPasswordWarning";
+            this.incorrectPasswordWarning.Size = new System.Drawing.Size(194, 19);
+            this.incorrectPasswordWarning.TabIndex = 6;
+            this.incorrectPasswordWarning.Text = "Incorrect! Try again";
+            this.incorrectPasswordWarning.Visible = false;
+            this.incorrectPasswordWarning.TextChanged += new System.EventHandler(this.incorrectPasswordWarning_TextChanged);
+            // 
+            // checkedListBoxRN
+            // 
+            this.checkedListBoxRN.FormattingEnabled = true;
+            this.checkedListBoxRN.Location = new System.Drawing.Point(35, 92);
+            this.checkedListBoxRN.Name = "checkedListBoxRN";
+            this.checkedListBoxRN.Size = new System.Drawing.Size(394, 717);
+            this.checkedListBoxRN.TabIndex = 7;
+            this.checkedListBoxRN.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1352, 889);
+            this.Controls.Add(this.checkedListBoxRN);
+            this.Controls.Add(this.incorrectPasswordWarning);
+            this.Controls.Add(this.restaurantNamesList);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.enterPassword);
@@ -102,6 +143,9 @@
         private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.ComboBox restaurantType;
         private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.ListBox restaurantNamesList;
+        private System.Windows.Forms.TextBox incorrectPasswordWarning;
+        private System.Windows.Forms.CheckedListBox checkedListBoxRN;
     }
 }
 
--- Foodreviews/Form1.cs
+++ Foodreviews/Form1.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Foodreviews
 {
@@ -16,48 +18,185 @@ namespace Foodreviews
         private const string CONNECT = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"M:\\Visual Studio 2022\\FOOD REVIEWS\\Foodreviews\\Foodreviews\\Food Reviews DDL.mdf\";Integrated Security=True;Connect Timeout=30";
         private SqlConnection sqlConnection;
 
+        List<object> restaurants = new List<object>();
+
+        private bool loggedIn;
+        private string username;
+
+
+        private void PopulateRestaurantList()
+        {
+            sqlConnection.Close();
+            sqlConnection.Open();
+
+            //setup query
+            SqlCommand command = new SqlCommand("SELECT RestaurantName FROM Restaurants ORDER BY RestaurantName", sqlConnection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                checkedListBoxRN.Items.Clear();
+                while (reader.Read())
+                {
+                    checkedListBoxRN.Items.Add(reader["RestaurantName"].ToString());
+                }
+            }
+            sqlConnection.Close();
+        }
+
         public Form1()
         {
             InitializeComponent();
             sqlConnection = new SqlConnection(CONNECT);
+            incorrectPasswordWarning.Visible = false;
+            restaurantNamesList.Visible = true;
+
+            sqlConnection.Open();
+
+            //setup query
+            SqlCommand command = new SqlCommand("SELECT * FROM Users", sqlConnection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if ((string)reader["UserName"] == enterUsername.Text || (string)reader["UserEmail"] == enterUsername.Text)
+                    {
+                        if ((string)reader["UserPassword"] == enterPassword.Text)
+                        {
+                            loggedIn = true;
+                            username = enterUsername.Text;
+                            break;
+                        }
+                    }
+                }
+            }
+            sqlConnection.Close();
+
+
+
+
         }
 
         private void GetTags()
         {
             sqlConnection.Open();
 
-            //setup query
+            //Tags query
             SqlCommand command = new SqlCommand("SELECT Tag FROM Tags ORDER BY Tag", sqlConnection);
 
             using (SqlDataReader reader = command.ExecuteReader())
             {
                 while (reader.Read())
                 {
-                    restaurantType.Items.Add(reader["Tag"]);
+                    restaurantType.Items.Add(reader["Tag"].ToString());
  
+                }
+            }
+
+            
+            //RESTAURANT NAME QUERY
+            SqlCommand commandRestaurants = new SqlCommand("SELECT * FROM Restaurants", sqlConnection);
+
+            using (SqlDataReader reader = commandRestaurants.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    restaurants.Add(reader["RestaurantName"].ToString());
                 }
             }
             sqlConnection.Close();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
+        public class restaurantDetails
+        {
+            public string RestaurantName { get; set; }
+            public string Tags { get; set; }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
         private void loginButton_Click(object sender, EventArgs e)
         {
-            if(enterUsername.Text == "admin" &&  enterPassword.Text == "1234")
+            sqlConnection.Open();
+
+            //setup query
+            SqlCommand command = new SqlCommand("SELECT * FROM Users", sqlConnection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                panelMain.Enabled = true;
-                
+                while (reader.Read())
+                {
+                    if ((string)reader["UserName"] == enterUsername.Text || (string)reader["UserEmail"] == enterUsername.Text)
+                    {
+                        if ((string)reader["UserPassword"] == enterPassword.Text)
+                        {
+                            loggedIn = true;
+                            username = enterUsername.Text;
+                            panelMain.Enabled = true;
+                            incorrectPasswordWarning.Visible = false;
+                            PopulateRestaurantList();
+                            break;
+                        }
+                        else if ((string)reader["UserPassword"] != enterPassword.Text)
+                        {
+                        
+                            incorrectPasswordWarning.Visible = true;
+                        }
+                    }
+                }
+            }
+            sqlConnection.Close();
+
+            if (loggedIn)
+            {
+                enterUsername.Hide();
+                enterPassword.Hide();
+                loginButton.Text = "Logout";
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //List<restaurantDetails> restaurants = new List<restaurantDetails>();
             GetTags();
             panelMain.Enabled = false;
+            restaurantNamesList.Enabled = false;
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enterUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void incorrectPasswordWarning_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restaurantType_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
